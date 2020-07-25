731b4068923371b6b4ebf4f22844e8e6acb348ee
Amitesh Kumar <Amitesh_Kumar@epam.com>
2020-07-25 11:19:16 +0530

    Creating components in different ways

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MessageElement></MessageElement>
+      <MessageComponent></MessageComponent>
+      <MessagePureComponent></MessagePureComponent>
+      <MessageFunctional></MessageFunctional>
     </div>
   );
+}
+
+/** Using functional component */
+function MessageFunctional() {
+  return (
+    <header className="App-message-container">
+      <h4 className="App-message-heading">Message created using <code>Functional component:</code></h4>
+      <span className="App-message">Hello World</span>
+    </header>
+  );
+}
+
+/** Using `React.Component` */
+class MessageComponent extends React.Component {
+  render() {
+    return (
+      <header className="App-message-container">
+        <h4 className="App-message-heading">Message created using <code>React.Component:</code></h4>
+        <span className="App-message">Hello World</span>
+      </header>
+    );
+  }
+}
+
+/** Using `React.PureComponent` */
+class MessagePureComponent extends React.PureComponent {
+  render() {
+    return (
+      <header className="App-message-container">
+        <h4 className="App-message-heading">Message created using <code>React.PureComponent:</code></h4>
+        <span className="App-message">Hello World</span>
+      </header>
+    );
+  }
+}
+
+/** Using `React.createElement` */
+class MessageElement extends React.Component {
+
+  render() {
+    return React.createElement("header", {
+      className: "App-message-container"
+    }, React.createElement("h4", {
+      className: "App-message-heading"
+    }, "Message created using ", React.createElement("code", null, "React.Component:")),
+      React.createElement("span", {
+        className: "App-message"
+      }, "Hello World"));
+  }
 }
 
 export default App;
